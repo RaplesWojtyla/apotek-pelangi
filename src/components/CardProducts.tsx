89a41430b45def5cd8c3e11d031776215b8d3848
Patra Raples Wojtyla Sinaga <144253449+RaplesWojtyla@users.noreply.github.com>
2025-05-20 02:34:08 +0700

    Update CardProducts.tsx

--- src/components/CardProducts.tsx
+++ src/components/CardProducts.tsx
@@ -1,54 +1,33 @@
+'use client'
+
 import React from "react";
 import ProductCard from "./ProductCard";
-
-const products = [
-		{
-			name: "Rhinus SR 60 Kapsul",
-			price: "Rp21.000/strip",
-			image: "/logo.png",
-		},
-		{
-			name: "Rhinofed 4.5mg/60ml",
-			price: "Rp21.500/botol",
-			image: "/logo.png",
-		},
-		{
-			name: "Termorex Sirup 60 ml",
-			price: "Rp8.000/botol",
-			image: "/logo.png",
-		},
-		{
-			name: "Termorex Sirup 60 ml",
-			price: "Rp8.000/botol",
-			image: "/logo.png",
-		},
-		{
-			name: "Rhinus SR 60 Kapsul",
-			price: "Rp21.000/strip",
-			image: "/logo.png",
-		},
-		{
-			name: "Rhinofed 4.5mg/60ml",
-			price: "Rp21.500/botol",
-			image: "/logo.png",
-		},
-		{
-			name: "Termorex Sirup 60 ml",
-			price: "Rp8.000/botol",
-			image: "/logo.png",
-		},
-		{
-			name: "Termorex Sirup 60 ml",
-			price: "Rp8.000/botol",
-			image: "/logo.png",
-		},
-	];
+import { useEffect, useState } from "react";
+import { ProductDetail } from "@/action/product.action";
 
 const CardProducts = () => {
+	const [page, setPage] = useState<number>(1)
+	const [search, setSearch] = useState<string>("")
+	const [products, setProducts] = useState<ProductDetail[]>([])
+
+	useEffect(() => {
+		const load = async () => {
+			const res = await fetch(`/api/product?page=${page}&search=${encodeURIComponent(search)}`)
+
+			if (!res.ok) throw new Error("Gagal mengambil data produk.")
+
+			const data: ProductDetail[] = await res.json()
+
+			setProducts(data)
+		}
+
+		load()
+	}, [page, search])
+	
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-			{products.map((product, i) => (
-				<ProductCard key={i} product={product} />
+			{products.map(product => (
+				<ProductCard key={product.id} product={product} />
 			))}
 		</div>
 	)
