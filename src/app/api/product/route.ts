c8836f1beda947f5c7179de7827c608d9a74740c
Patra Raples Wojtyla Sinaga <rapleswojtyla@gmail.com>
2025-05-21 11:48:52 +0700

    Type error at product route: Expected 1 arguments, but got 3.

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -7,7 +7,7 @@ export async function GET(req: Request) {
 		const page = Number(searchParams.get('page') ?? 1) 
 		const search = String(searchParams.get('search') ?? "") 
 		const take = Number(searchParams.get('take') ?? 16)
-		const products = await getProducts(page, search, take)
+		const products = await getProducts({page: page, matcher: search, take: take})
 
 		return NextResponse.json(products)
 	} catch (error: any) {
